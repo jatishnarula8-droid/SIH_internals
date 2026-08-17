8cfecc334d1d03e66fe0909372ffa78c9b0ed89c
ravidrolia-dev <ravi06drolia@gmail.com>
2026-08-18 03:25:43 +0530

    Added pest engine

--- ml-api/main.py
+++ ml-api/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from routers.health import router as health_router
+from routers.pest import router as pest_router
 
 app = FastAPI(title="किसानसारथी")
 
@@ -15,6 +16,7 @@ app.add_middleware(
 )
 
 app.include_router(health_router)
+app.include_router(pest_router)
 
 @app.get("/")
 def read_root():
